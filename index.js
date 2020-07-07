31084a9206ab600369189fd741548ac450b346d8
0x0a0d <vandug23@gmail.com>
2020-07-07 21:20:05 +0700

    remove debug

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function getSameLength(strs) {
   let j = 0;
   for (; j < strs[0].length; j++) {
     for (let i = 0; i < strs.length - 1; i++) {
-      console.log(i, j, strs[i][j], strs[i + 1][j])
       if (strs[i][j] !== strs[i + 1][j]) return j - 1
     }
   }
