6ca11e43f05eea96509273cf599c3b352c541f9f
0x0a0d <vandug23@gmail.com>
2020-07-07 21:14:39 +0700

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+function getSameLength(strs) {
+  let j = 0;
+  for (; j < strs[0].length; j++) {
+    for (let i = 0; i < strs.length - 1; i++) {
+      console.log(i, j, strs[i][j], strs[i + 1][j])
+      if (strs[i][j] !== strs[i + 1][j]) return j - 1
+    }
+  }
+  return j
+}
+
+function getSameString(strs) {
+  const sameLength = getSameLength(strs)
+  return strs[0].substr(0, sameLength)
+}
+
+module.exports = {
+  getSameString,
+  getSameLength
+}
